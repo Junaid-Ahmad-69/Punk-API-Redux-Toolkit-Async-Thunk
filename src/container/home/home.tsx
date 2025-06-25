8dd3cf8ca6f4ed7a024978dd7db8c4332e4e742d
junaid-sv <junaid.ahmad@southvillesol.com>
2025-06-25 16:14:33 +0500

    added filters half

--- src/container/home/home.tsx
+++ src/container/home/home.tsx
@@ -8,16 +8,38 @@ import PaginationList from "@/components/Pagination";
 import {HomeHeading} from "../../../constants/data.tsx";
 import {useNavigate} from "react-router";
 import {ViewBeerDetail} from "../../../Routes/Route.tsx";
-import {setPage} from "@/features/beer/slice.ts";
+import {setABV, setPage, setIBU} from "@/features/beer/slice.ts";
+import Filters from "@/components/Filters";
 
-const Home = ()=> {
+
+const Home = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch<AppDispatch>();
-    const {loading, error, page, data} = useSelector((state: RootState) => state.beer);
+    const {loading, error, page, data, limit, totalCount, abv_gt, ibu_gt} = useSelector((state: RootState) => state.beer);
+
 
     useEffect(() => {
-        dispatch(fetchBeers({page, per_page: 10}))
-    }, [dispatch, page])
+        const handler = setTimeout(() => {
+            // const abvValue = abv_gt?.toString().trim();
+            if (abv_gt !== 0 && abv_gt || ibu_gt !== 0 && ibu_gt ) {
+                dispatch(fetchBeers({
+                    page,
+                    per_page: limit,
+                    abv_gt,
+                    ibu_gt,
+                }));
+            } else {
+                dispatch(fetchBeers({
+                    page,
+                    per_page: limit
+                }));
+            }
+        }, 500);
+
+        return () => clearTimeout(handler);
+    }, [ibu_gt, abv_gt, page, limit]);
+
+
 
     if (loading) {
         return <Loader/>
@@ -26,19 +48,40 @@ const Home = ()=> {
         return <div>Error: {error}</div>;
     }
     const handleChange = (value: number) => dispatch(setPage(value))
+    const handleNavigate = (id: string) => navigate(ViewBeerDetail.replace(":id", id))
 
-    const handleNavigate= (id: string)=> navigate(ViewBeerDetail.replace(":id", id ))
     return (
         <div className="container mx-auto py-24">
-            <DataTable heading={HomeHeading} list={data} handleRowClick={handleNavigate} />
+            <Filters
+                data={[
+                    {
+                        type: 'number',
+                        name: 'ABV',
+                        label: 'Search By ABV',
+                        placeholder: 'Search by ABV....',
+                        value: abv_gt,
+                        handleChangeFilter: (value: string) => dispatch(setABV(value)),
+                    },
+                    {
+                        type: 'number',
+                        name: 'IBU',
+                        label: 'Search By IBU',
+                        placeholder: 'Search by IBU....',
+                        value: ibu_gt,
+                        handleChangeFilter: (value: string) => dispatch(setIBU(value)),
+                    }
+                ]}
+            />
+            <DataTable heading={HomeHeading} list={data} handleRowClick={handleNavigate}/>
             <div className="my-12">
-                {data.length > ( data.length - 1 ) &&
+                {totalCount > limit &&
                     <PaginationList
-                        limit={10}
+                        limit={limit}
                         defaultPage={page}
-                        totalCount={420}
+                        totalCount={totalCount}
                         handleChange={handleChange}
-                    />}
+                    />
+                }
             </div>
         </div>
     )
